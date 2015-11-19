edcaa5c1033fc0306a35c80f6b52b8dfacb3aa69
Andrea Parenti <parenti@xfel.eu>
2015-11-19 14:03:51 +0000

    Removed 'import *' statement

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ __date__ ="$Jul 17, 2013 2:46:38 PM$"
 __copyright__="Copyright (c) 2010-2013 European XFEL GmbH Hamburg. All rights reserved."
 
 from karabo.configurator import Configurator
-from ImageProcessor import *
+from karabo.karathon import Hash
+from ImageProcessor import PythonDevice
 
 if __name__ == "__main__":
     device = Configurator(PythonDevice).create("ImageProcessor", Hash("Logger.priority", "DEBUG", "deviceId", "ImageProcessorMain_0"))
